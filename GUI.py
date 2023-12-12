154088e4fee42e0cc0fcd74b03f050db52b11252
rami3564 <130843194+rami3564@users.noreply.github.com>
2023-12-12 22:48:42 +0800

    update

--- GUI.py
+++ GUI.py
@@ -6,7 +6,7 @@ Created on Fri Apr 30 13:36:58 2021
 @author: bing
 """
 
-# import all the required  modules
+# import all the required modules
 import threading
 import select
 from tkinter import *
@@ -28,11 +28,9 @@ from tkinter import messagebox
 from tkinter import filedialog
 import random
 
-
-
 # GUI class for the chat
 class GUI:
-    def __init__(self, send, recv, sm, s):
+    def __init__(self, send=None, recv=None, sm=None, s=None):
         self.Window = Tk()
         self.Window.withdraw()
         self.send = send
@@ -50,15 +48,12 @@ class GUI:
         self.login.title("Login")
         self.login.geometry('450x300')
 
-        
-
-        self.pls = Label(self.login, text="Please login to continue", font="Comic Sans MS", size=14, weight="bold")
+        self.pls = Label(self.login, text="Please login to continue", font=("Comic Sans MS", 14, "bold"))
         self.pls.place(x=60, y=65)
-        
 
-        self.labelName = Label(self.login, text="Username: ", font="Comic Sans MS", size=12, bg="white")
+        self.labelName = Label(self.login, text="Username: ")
         self.labelName.place(x=90, y=130)
-        self.labelPwd = Label(self.login, text="Password: ", font="Comic Sans MS", size=12, bg="white")
+        self.labelPwd = Label(self.login, text="Password: ", font=("Comic Sans MS", 12, "bold"))
         self.labelPwd.place(x=90, y=170)
 
         self.var_usr_name = StringVar()
@@ -100,9 +95,9 @@ class GUI:
                 self.gopage = Toplevel(self.login)
                 self.gopage.title("CHATROOM")
                 self.gopage.geometry('450x300')
-                self.wel = Label(self.gopage, text="Welcome to Our Chatroom!", font="Comic Sans MS", size=14, weight="bold")
+                self.wel = Label(self.gopage, text="Welcome to Our Chatroom!", font=("Comic Sans MS", 14, "bold"))
                 self.wel.place(x=60, y=65)
-                self.go = Button(self.gopage, text="CONTINUE",font="Comic Sans MS", size=14, weight="bold", command=lambda: self.goAhead(self.login_name))
+                self.go = Button(self.gopage, text="CONTINUE", font=("Comic Sans MS", 14, "bold"), command=lambda: self.goAhead(self.login_name))
                 self.go.place(x=80, y=120)
             else:
                 messagebox.showerror(message='Incorrect password.')
@@ -140,9 +135,9 @@ class GUI:
                 self.gopage = Toplevel(self.login)
                 self.gopage.title("CHATROOM")
                 self.gopage.geometry('450x300')
-                self.wel = Label(self.gopage, text="Welcome to Our Chatroom!", font="Comic Sans MS", size=14, weight="bold")
+                self.wel = Label(self.gopage, text="Welcome to Our Chatroom!", font=("Comic Sans MS", 14, "bold"))
                 self.wel.place(x=60, y=65)
-                self.go = Button(self.gopage, text="CONTINUE", font="Comic Sans MS", size=14, weight="bold", command=lambda: self.goAhead(self.login_name))
+                self.go = Button(self.gopage, text="CONTINUE", font=("Comic Sans MS", 14, "bold"), command=lambda: self.goAhead(self.login_name))
                 self.go.place(x=80, y=120)
 
         window_sign_up = Toplevel(self.login)
@@ -165,14 +160,13 @@ class GUI:
         bt_confirm_sign_up.place(x=150, y=130)
 
         window_sign_up.mainloop()
-    
-    
+
     def usr_sign_quit(self):
         self.login.destroy()
-    
+
     def goAhead(self, name):
         if len(name) > 0:
-            msg = json.dumps({"action":"login", "name": name})
+            msg = json.dumps({"action": "login", "name": name})
             self.send(msg)
             response = json.loads(self.recv())
             if response["status"] == 'ok':
@@ -180,132 +174,68 @@ class GUI:
                 self.sm.set_state(S_LOGGEDIN)
                 self.sm.set_myname(name)
                 self.layout(name)
-                self.textCons.config(state = NORMAL)
-                # self.textCons.insert(END, "hello" +"\n\n")   
-                self.textCons.insert(END, menu +"\n\n")      
-                self.textCons.config(state = DISABLED)
+                self.textCons.config(state=NORMAL)
+                self.textCons.insert(END, menu + "\n\n")
+                self.textCons.config(state=DISABLED)
                 self.textCons.see(END)
-                # while True:
-                #     self.proc()
-        # the thread to receive messages
+            # the thread to receive messages
             process = threading.Thread(target=self.proc)
             process.daemon = True
-            process.start()  
+            process.start()
+
     # The main layout of the chat
-    def layout(self,name):
-        
+    def layout(self, name):
         self.name = name
         # to show chat window
         self.Window.deiconify()
         self.Window.title("CHATROOM")
-        self.Window.resizable(width = False,
-                              height = False)
-        self.Window.configure(width = 470,
-                              height = 550,
-                              bg = "#17202A")
-        self.labelHead = Label(self.Window,
-                             bg = "#17202A", 
-                              fg = "#EAECEE",
-                              text = self.name ,
-                               font="Comic Sans MS", size=13, weight="bold",
-                               pady = 5)
-          
-        self.labelHead.place(relwidth = 1)
-        self.line = Label(self.Window,
-                          width = 450,
-                          bg = "#ABB2B9")
-          
-        self.line.place(relwidth = 1,
-                        rely = 0.07,
-                        relheight = 0.012)
-          
-        self.textCons = Text(self.Window,
-                             width = 20, 
-                             height = 2,
-                             bg = "#17202A",
-                             fg = "#EAECEE",
-                             font="Comic Sans MS", size=14, 
-                             padx = 5,
-                             pady = 5)
-          
-        self.textCons.place(relheight = 0.745,
-                            relwidth = 1, 
-                            rely = 0.08)
-          
-        self.labelBottom = Label(self.Window,
-                                 bg = "#ABB2B9",
-                                 height = 80)
-          
-        self.labelBottom.place(relwidth = 1,
-                               rely = 0.825)
-          
-        self.entryMsg = Entry(self.labelBottom,
-                              bg = "#2C3E50",
-                              fg = "#EAECEE",
-                              font="Comic Sans MS", size=13)
-          
-        # place the given widget
-        # into the gui window
-        self.entryMsg.place(relwidth = 0.74,
-                            relheight = 0.06,
-                            rely = 0.008,
-                            relx = 0.011)
-          
+        self.Window.resizable(width=False, height=False)
+        self.Window.configure(width=470, height=550, bg="#17202A")
+        self.labelHead = Label(self.Window, bg="#17202A", fg="#EAECEE", text=self.name,
+                               font=("Comic Sans MS", 13, "bold"), pady=5)
+        self.labelHead.place(relwidth=1)
+        self.line = Label(self.Window, width=450, bg="#ABB2B9")
+        self.line.place(relwidth=1, rely=0.07, relheight=0.012)
+        self.textCons = Text(self.Window, width=20, height=2, bg="#17202A", fg="#EAECEE",
+                             font=("Comic Sans MS", 14), padx=5, pady=5)
+        self.textCons.place(relheight=0.745, relwidth=1, rely=0.08)
+        self.labelBottom = Label(self.Window, bg="#ABB2B9", height=80)
+        self.labelBottom.place(relwidth=1, rely=0.825)
+        self.entryMsg = Entry(self.labelBottom, bg="#2C3E50", fg="#EAECEE", font=("Comic Sans MS", 13))
+        self.entryMsg.place(relwidth=0.74, relheight=0.06, rely=0.008, relx=0.011)
         self.entryMsg.focus()
-          
-        # create a Send Button
-        self.buttonMsg = Button(self.labelBottom,
-                                text = "Send",
-                                font="Comic Sans MS", size=10, weight="bold", 
-                                width = 20,
-                                bg = "#ABB2B9",
-                                command = lambda : self.sendButton(self.entryMsg.get()))
-          
-        self.buttonMsg.place(relx = 0.77,
-                             rely = 0.008,
-                             relheight = 0.06, 
-                             relwidth = 0.22)
-          
-        self.textCons.config(cursor = "arrow")
-          
-        # create a scroll bar
+        self.buttonMsg = Button(self.labelBottom, text="Send", font=("Comic Sans MS", 10, "bold"),
+                                width=20, bg="#ABB2B9", command=lambda: self.sendButton(self.entryMsg.get()))
+        self.buttonMsg.place(relx=0.77, rely=0.008, relheight=0.06, relwidth=0.22)
+        self.textCons.config(cursor="arrow")
         scrollbar = Scrollbar(self.textCons)
-          
-        # place the scroll bar 
-        # into the gui window
-        scrollbar.place(relheight = 1,
-                        relx = 0.974)
-          
-        scrollbar.config(command = self.textCons.yview)
-          
-        self.textCons.config(state = DISABLED)
-  
-    # function to basically start the thread for sending messages
+        scrollbar.place(relheight=1, relx=0.974)
+        scrollbar.config(command=self.textCons.yview)
+        self.textCons.config(state=DISABLED)
+
     def sendButton(self, msg):
-        self.textCons.config(state = DISABLED)
+        self.textCons.config(state=DISABLED)
         self.my_msg = msg
-        # print(msg)
         self.entryMsg.delete(0, END)
 
     def proc(self):
-        # print(self.msg)
         while True:
             read, write, error = select.select([self.socket], [], [], 0)
             peer_msg = []
-            # print(self.msg)
             if self.socket in read:
                 peer_msg = self.recv()
             if len(self.my_msg) > 0 or len(peer_msg) > 0:
-                # print(self.system_msg)
                 self.system_msg += self.sm.proc(self.my_msg, peer_msg)
                 self.my_msg = ""
-                self.textCons.config(state = NORMAL)
-                self.textCons.insert(END, self.system_msg +"\n\n")      
-                self.textCons.config(state = DISABLED)
+                self.textCons.config(state=NORMAL)
+                self.textCons.insert(END, self.system_msg + "\n\n")
+                self.textCons.config(state=DISABLED)
                 self.textCons.see(END)
 
     def run(self):
         self.login()
+
 # create a GUI class object
-if __name__ == "__main__": 
+if __name__ == "__main__":
     g = GUI()
+    
